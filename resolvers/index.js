1f7e7b6d09330ea6839bee3d8e94bd331d864bcb
IsmatAyash <ismat.ayash@gmail.com>
2021-12-31 17:55:03 +0200

    removed product model

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,4 +1,3 @@
 import book from "./book.js";
-import product from "./product.js";
 
-export default [book, product];
+export default [book];
--- typeDefs/index.js
+++ typeDefs/index.js
@@ -1,5 +1,4 @@
 import baseDefs from "./baseDefs.js";
 import book from "./book.js";
-import product from "./product.js";
 
-export default [baseDefs, book, product];
+export default [baseDefs, book];
