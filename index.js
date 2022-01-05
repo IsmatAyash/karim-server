757d69431f51c38a8724c6f882a6fd2460cab64f
IsmatAyash <ismat.ayash@gmail.com>
2022-01-05 16:32:10 +0200

    Added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import express from "express";
 import { createServer } from "http";
 import expressJwt from "express-jwt";
-import express from "express";
+import cors from "cors";
 
 import { SECRET } from "./config/index.js";
 import startdb from "./startup/startdb.js";
@@ -8,6 +9,16 @@ import startApolloServer from "./startup/startApolloServer.js";
 
 const app = express();
 const httpServer = createServer(app);
+const corsConfig = cors({
+  origin: [
+    "http://localhost:8000",
+    "https://studio.apollographql.com",
+    "https://karimland.netlify.app",
+  ],
+  credentials: true,
+});
+app.use(corsConfig);
+
 const jwt = expressJwt({
   secret: SECRET,
   algorithms: ["HS256"],
